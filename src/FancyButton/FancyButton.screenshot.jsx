d4c2f97309784cce429eca411767d9d85bace7c5
Arnaud Crowther <arnaudcrowther@gmail.com>
2022-02-15 15:41:00 -0500

    added jest.screenshot files

---
+++ src/FancyButton/FancyButton.screenshot.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { ReactScreenshotTest } from "react-screenshot-test";
+import { FancyButton } from "./FancyButton";
+import "../index.css";
+import { redLabel } from "./style.module.css";
+
+describe("screenshots", () => {
+  ReactScreenshotTest.create("FancyButton")
+    .viewport("Desktop", {
+      width: 1024,
+      height: 768,
+    })
+    .viewport("iPhone X", {
+      width: 375,
+      height: 812,
+      deviceScaleFactor: 3,
+      isMobile: true,
+      hasTouch: true,
+      isLandscape: false,
+    })
+    .shoot("with label", <FancyButton label="Hello, World!" />)
+    .shoot("empty label", <FancyButton />)
+    .shoot(
+      "custom label",
+      <FancyButton label={<span className={redLabel}>Red label</span>} />
+    )
+    .run();
+});
